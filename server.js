f251a90139e2a6ef3adba7767171cfa10d8319f1
Darren Behan <darrenbehan@hotmail.com>
2020-09-03 18:29:36 +1000

    installed dependencies, setup express & mongoose

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/golferschoice", { useNewUrlParser: true });
+
+// Routes
+// require("./routes/api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port http://localhost:${PORT}`);
+});

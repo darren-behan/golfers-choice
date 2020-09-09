3fd958c4333a5780d9676282ab80997b3c188577
Darren Behan <darrenbehan@hotmail.com>
2020-09-09 16:43:11 +1000

    adding UserSchema

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  user: [
+    {
+      first_name: {
+        type: String,
+        trim: true,
+        required: "Enter your first name"
+      },
+      last_name: {
+        type: String,
+        trim: true,
+        required: "Enter your last name"
+      },
+      email: {
+        type: String,
+        trim: true,
+        required: "Enter your email"
+      },
+      password: {
+        type: String,
+        trim: true,
+        required: "Enter your password"
+      }
+    }
+  ]
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;

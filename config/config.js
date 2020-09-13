7fc12f33933048b40c8d80be262024936f0748a2
Darren Behan <darrenbehan@hotmail.com>
2020-09-13 11:52:55 +1000

    further congfiguration to eslint

---
+++ config/config.js
@@ -0,0 +1,27 @@
+// For security, create a ".env" file in the project folder using the template in the existing ".env.sample" file,
+// inputting all the necessary parameters. ".gitignore" will ensure that these details are never pushed up to GitHub.
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.PORT,
+    dialect: process.env.MONGODB,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.PORT,
+    dialect: process.env.MONGODB,
+  },
+  production: {
+    useEnvVariable: "MONGODB_URI",
+    dialect: "mongodb",
+  },
+};

ad59c49061fc3afd747ed127ad0038e8feb5ed96
Darren Behan <darrenbehan@hotmail.com>
2020-09-27 18:44:47 +1000

    updating html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,7 +7,12 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads index.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/index.html"));
+  // });
+
+  // If no API routes are hit, send the React app
+  router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 };

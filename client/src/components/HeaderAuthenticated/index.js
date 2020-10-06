1a7c4f653cf3508cb40d23bfbff579638a68a877
Darren Behan <darrenbehan@hotmail.com>
2020-10-07 09:20:40 +1100

    added function for when user logs out it sets isAuthenticated to false and redirects to home

--- client/src/components/HeaderAuthenticated/index.js
+++ client/src/components/HeaderAuthenticated/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import "./index.css";
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import DataAreaContext from "../../utils/DataAreaContext";
 
-function HeaderAuthenticated(props) {
+function HeaderAuthenticated() {
+  const { setIsAuthenticated } = useContext(DataAreaContext);
+
+  function handleOnClick() {
+    setIsAuthenticated(false);
+  };
 
   return(
     <DropdownButton
@@ -18,7 +24,7 @@ function HeaderAuthenticated(props) {
       <Dropdown.Item eventKey="2" href="reset-password">Reset password</Dropdown.Item>
       <Dropdown.Item eventKey="3" href="delete-account">Delete account</Dropdown.Item>
       <Dropdown.Divider />
-      <Dropdown.Item eventKey="4" href="logout">Logout</Dropdown.Item>
+      <Dropdown.Item eventKey="4" href="home" onClick={ handleOnClick }>Logout</Dropdown.Item>
     </DropdownButton>
   )
 }

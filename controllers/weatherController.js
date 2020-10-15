e3827ef59dd880cc78d334cc46e4bf1def86d935
Darren Behan <darrenbehan@hotmail.com>
2020-10-15 20:07:48 +1100

    added ', IE' to weather API URL so it returns exact county in Ireland

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 module.exports = {
   findByCounty: function(req, res) {
     // We use axios to submit our GET request to the OpenWeather API
-    axios.get("https://api.openweathermap.org/data/2.5/weather?q=" + req.params.county + "&units=metric&appid=" + `${process.env.WEATHER_API_KEY}`)
+    axios.get("https://api.openweathermap.org/data/2.5/weather?q=" + req.params.county + ", IE" + "&units=metric&appid=" + `${process.env.WEATHER_API_KEY}`)
     .then(
       // Once the request is complete, we run a callback function so our result is passed back to our front end through the response of the API call and in a JSON format
       function(result) {
